7bc0ad24e9274862f623fe39304928bfe355ccea
Danny Phillips <dannyp@procore.com>
2017-11-26 02:11:18 -0800

    add PlayerCard

---
+++ packages/drone/components/PlayerCard.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import {
+  Body,
+  Button,
+  Left,
+  Right,
+  ListItem,
+  Icon,
+  Text,
+  Thumbnail
+} from "native-base";
+import player from "../assets/images/robot-dev.png";
+
+export default class PlayerCard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { name, battleCry, thumbnail } = this.props;
+    return (
+      <ListItem>
+        <Left>
+          <Thumbnail square size={80} source={thumbnail} />
+          <Body>
+            <Text>{name}</Text>
+            <Text note>{battleCry}</Text>
+          </Body>
+        </Left>
+        <Right>
+          <Button transparent>
+            <Icon active name="information-circle" />
+          </Button>
+        </Right>
+      </ListItem>
+    );
+  }
+}
+
+PlayerCard.defaultProps = {
+  name: "Danny Phillips",
+  battleCry: "This is a battleCry",
+  thumbnail: player
+};
+
+const styles = StyleSheet.create({});
